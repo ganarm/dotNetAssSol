64a56afeb68c373b8eec57b6a8a3f4169b99d067
GANESH RAJENDRA MALI <156067355+ganarm@users.noreply.github.com>
2025-04-07 00:17:00 +0530

    Add files via upload

---
+++ RailwayReservation-7a/RailwayReservation-7a/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace RailwayReservation_7a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassengerName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.fontDialog2 = new System.Windows.Forms.FontDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cmbTrain = new System.Windows.Forms.ComboBox();
+            this.cmbClass = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtFare = new System.Windows.Forms.TextBox();
+            this.btnCalculateFare = new System.Windows.Forms.Button();
+            this.btnBookTicket = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passenger Name:";
+            // 
+            // txtPassengerName
+            // 
+            this.txtPassengerName.Location = new System.Drawing.Point(136, 51);
+            this.txtPassengerName.Name = "txtPassengerName";
+            this.txtPassengerName.Size = new System.Drawing.Size(161, 22);
+            this.txtPassengerName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(136, 84);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(161, 22);
+            this.txtAge.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gender:";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.cmbGender.Location = new System.Drawing.Point(136, 117);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(161, 24);
+            this.cmbGender.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(370, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Passenger Information Section";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(295, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = " Journey Details Section";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "From:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 255);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "To:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Date of Journey:";
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Items.AddRange(new object[] {
+            "Mumbai",
+            "Pune",
+            "Delhi",
+            "Kolkata",
+            "Chennai",
+            "Bangalore"});
+            this.cmbFrom.Location = new System.Drawing.Point(136, 209);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(192, 24);
+            this.cmbFrom.TabIndex = 11;
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Items.AddRange(new object[] {
+            "Mumbai",
+            "Pune",
+            "Delhi",
+            "Kolkata",
+            "Chennai",
+            "Bangalore"});
+            this.cmbTo.Location = new System.Drawing.Point(136, 247);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(192, 24);
+            this.cmbTo.TabIndex = 12;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(136, 291);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(192, 22);
+            this.dtpDate.TabIndex = 13;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 450);
+            this.splitter1.TabIndex = 14;
+            this.splitter1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(477, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(286, 29);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Train Selection Section";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(482, 56);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 16);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Select Train:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(485, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 16);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Class:";
+            // 
+            // cmbTrain
+            // 
+            this.cmbTrain.FormattingEnabled = true;
+            this.cmbTrain.Items.AddRange(new object[] {
+            "Rajdhani Express",
+            "Shatabdi Express",
+            "Duronto Express",
+            "Garib Rath",
+            "Tejas Express"});
+            this.cmbTrain.Location = new System.Drawing.Point(570, 49);
+            this.cmbTrain.Name = "cmbTrain";
+            this.cmbTrain.Size = new System.Drawing.Size(121, 24);
+            this.cmbTrain.TabIndex = 18;
+            // 
+            // cmbClass
+            // 
+            this.cmbClass.FormattingEnabled = true;
+            this.cmbClass.Items.AddRange(new object[] {
+            "First AC",
+            "Second AC",
+            "Third AC",
+            "Sleeper"});
+            this.cmbClass.Location = new System.Drawing.Point(570, 89);
+            this.cmbClass.Name = "cmbClass";
+            this.cmbClass.Size = new System.Drawing.Size(121, 24);
+            this.cmbClass.TabIndex = 19;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(482, 171);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(263, 29);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Ticket Price & Booking";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(482, 216);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(72, 16);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Total Fare:";
+            // 
+            // txtFare
+            // 
+            this.txtFare.Location = new System.Drawing.Point(570, 216);
+            this.txtFare.Name = "txtFare";
+            this.txtFare.ReadOnly = true;
+            this.txtFare.Size = new System.Drawing.Size(121, 22);
+            this.txtFare.TabIndex = 22;
+            // 
+            // btnCalculateFare
+            // 
+            this.btnCalculateFare.Location = new System.Drawing.Point(559, 255);
+            this.btnCalculateFare.Name = "btnCalculateFare";
+            this.btnCalculateFare.Size = new System.Drawing.Size(132, 23);
+            this.btnCalculateFare.TabIndex = 23;
+            this.btnCalculateFare.Text = "Calculate Fare";
+            this.btnCalculateFare.UseVisualStyleBackColor = true;
+            this.btnCalculateFare.Click += new System.EventHandler(this.btnCalculateFare_Click);
+            // 
+            // btnBookTicket
+            // 
+            this.btnBookTicket.Location = new System.Drawing.Point(559, 303);
+            this.btnBookTicket.Name = "btnBookTicket";
+            this.btnBookTicket.Size = new System.Drawing.Size(132, 23);
+            this.btnBookTicket.TabIndex = 24;
+            this.btnBookTicket.Text = "Book Ticket";
+            this.btnBookTicket.UseVisualStyleBackColor = true;
+            this.btnBookTicket.Click += new System.EventHandler(this.btnBookTicket_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(559, 344);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 23);
+            this.btnClear.TabIndex = 25;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnBookTicket);
+            this.Controls.Add(this.btnCalculateFare);
+            this.Controls.Add(this.txtFare);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.cmbClass);
+            this.Controls.Add(this.cmbTrain);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.cmbTo);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtPassengerName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "la";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassengerName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.FontDialog fontDialog2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cmbTrain;
+        private System.Windows.Forms.ComboBox cmbClass;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtFare;
+        private System.Windows.Forms.Button btnCalculateFare;
+        private System.Windows.Forms.Button btnBookTicket;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ RailwayReservation-7a/RailwayReservation-7a/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RailwayReservation_7a
+{
+    public partial class Form1 : Form
+    {
+
+        private double baseFare;
+        private double totalFare;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculateFare_Click(object sender, EventArgs e)
+        {
+            if (cmbTrain.SelectedItem == null || cmbClass.SelectedItem == null)
+            {
+                MessageBox.Show("Please select Train and Class.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            switch (cmbTrain.SelectedItem.ToString())
+            {
+                case "Rajdhani Express":
+                    baseFare = 1500;
+                    break;
+                case "Shatabdi Express":
+                    baseFare = 1200;
+                    break;
+                case "Duronto Express":
+                    baseFare = 1800;
+                    break;
+                case "Garib Rath":
+                    baseFare = 800;
+                    break;
+                case "Tejas Express":
+                    baseFare = 2000;
+                    break;
+            }
+
+            switch (cmbClass.SelectedItem.ToString())
+            {
+                case "First AC":
+                    totalFare = baseFare * 2.5;
+                    break;
+                case "Second AC":
+                    totalFare = baseFare * 2.0;
+                    break;
+                case "Third AC":
+                    totalFare = baseFare * 1.5;
+                    break;
+                case "Sleeper":
+                    totalFare = baseFare * 1.0;
+                    break;
+            }
+
+            txtFare.Text = totalFare.ToString("C"); // Format as currency
+        }
+
+        private void btnBookTicket_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPassengerName.Text) || string.IsNullOrEmpty(txtAge.Text) ||
+                cmbGender.SelectedItem == null || cmbFrom.SelectedItem == null || cmbTo.SelectedItem == null ||
+                cmbTrain.SelectedItem == null || cmbClass.SelectedItem == null || string.IsNullOrEmpty(txtFare.Text))
+            {
+                MessageBox.Show("Please fill all fields before booking.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("Ticket Booked Successfully!\n\n" +
+                            $"Passenger: {txtPassengerName.Text}\n" +
+                            $"Age: {txtAge.Text}\n" +
+                            $"Gender: {cmbGender.SelectedItem}\n" +
+                            $"From: {cmbFrom.SelectedItem}\n" +
+                            $"To: {cmbTo.SelectedItem}\n" +
+                            $"Train: {cmbTrain.SelectedItem}\n" +
+                            $"Class: {cmbClass.SelectedItem}\n" +
+                            $"Fare: {txtFare.Text}\n" +
+                            $"Date of Journey: {dtpDate.Value.ToShortDateString()}",
+                            "Booking Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtPassengerName.Clear();
+            txtAge.Clear();
+            cmbGender.SelectedIndex = -1;
+            cmbFrom.SelectedIndex = -1;
+            cmbTo.SelectedIndex = -1;
+            cmbTrain.SelectedIndex = -1;
+            cmbClass.SelectedIndex = -1;
+            txtFare.Clear();
+        }
+
+    }
+}
